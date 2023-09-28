01d857518881b4abb7f1c76dcb7246442ab21bcd
viny <vinipires2013@gmail.com>
2023-09-28 13:24:58 -0300

    atualiza o geraconfig

--- geraconfig.sh
+++ geraconfig.sh
@@ -1,12 +1,12 @@
 
 echo '{                                                     ' >  config/microservices.json
 echo '  "models": {                                         ' >> config/microservices.json
-echo '    "modelo01": {                                     ' >> config/microservices.json
+echo '    "modelo_kmeans": {                                     ' >> config/microservices.json
 echo '      "version": "V01",                               ' >> config/microservices.json
 echo '      "url": "http://'$(sudo docker inspect serving01 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
 ['plat_network']['IPAddress'])")':8080/predict"          ' >> config/microservices.json
 echo '    },                                                ' >> config/microservices.json
-echo '    "modelo02": {                                     ' >> config/microservices.json
+echo '    "modelo_previsao": {                                     ' >> config/microservices.json
 echo '      "version": "V01",                               ' >> config/microservices.json
 echo '      "url": "http://'$(sudo docker inspect serving02 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
 ['plat_network']['IPAddress'])")':8080/predict"          ' >> config/microservices.json
