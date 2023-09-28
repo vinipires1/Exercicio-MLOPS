b5939a56ce330eddf6400964ed9323534b9b8ca7
viny <vinipires2013@gmail.com>
2023-09-28 17:24:30 -0300

    acho que agr vai

--- geraconfig.sh
+++ geraconfig.sh
@@ -4,12 +4,12 @@ echo '  "models": {                                         ' >> config/microser
 echo '    "modelo_kmeans": {                                     ' >> config/microservices.json
 echo '      "version": "V01",                               ' >> config/microservices.json
 echo '      "url": "http://'$(sudo docker inspect serving01 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
-['plat_network']['IPAddress'])")':8080/predict"          ' >> config/microservices.json
+['plat_network']['IPAddress'])")':8080/modelo_kmeans"          ' >> config/microservices.json
 echo '    },                                                ' >> config/microservices.json
 echo '    "modelo_regressao": {                                     ' >> config/microservices.json
 echo '      "version": "V01",                               ' >> config/microservices.json
 echo '      "url": "http://'$(sudo docker inspect serving02 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
-['plat_network']['IPAddress'])")':8080/predict"          ' >> config/microservices.json
+['plat_network']['IPAddress'])")':8080/modelo_regressao"          ' >> config/microservices.json
 echo '    }                                                 ' >> config/microservices.json
 echo '  }                                                   ' >> config/microservices.json
 echo '}                                                     ' >> config/microservices.json
