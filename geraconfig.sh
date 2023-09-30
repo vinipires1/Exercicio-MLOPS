2b127262e1345bb4b40156bda2268e2beeb205bc
Marcus Vinicius Pires <vinipires2013@gmail.com>
2023-09-29 21:36:35 -0300

    '-'

--- geraconfig.sh
+++ geraconfig.sh
@@ -11,7 +11,7 @@ echo '      "version": "V01",                               ' >> config/microser
 echo '      "url": "http://'$(sudo docker inspect serving02 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
 ['plat_network']['IPAddress'])")':8080/modelo_regressao"       ' >> config/microservices.json
 echo '    },                                                ' >> config/microservices.json
-echo '    "modelo_xgb": {                               ' >> config/microservices.json
+echo '    "modelo_xgb": {                                        ' >> config/microservices.json
 echo '      "version": "V01",                               ' >> config/microservices.json
 echo '      "url": "http://'$(sudo docker inspect serving03 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
 ['plat_network']['IPAddress'])")':8080/modelo_xgb"             ' >> config/microservices.json
